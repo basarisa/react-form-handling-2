3e3688e1fb81a7ff5fd700898f36c4afe628f4df
Sappakit <pipitwattana.s@gmail.com>
2024-10-23 15:48:26 +0700

    feat: complete exercise

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 function ProductForm() {
+  const [inputName, setInputName] = useState("");
+  const [inputImage, setInputImage] = useState("");
+  const [inputPrice, setInputPrice] = useState(0);
+  const [inputDescription, setInputDescription] = useState("");
+
+  function handleSubmit() {
+    const inputData = {
+      name: inputName,
+      price: inputPrice,
+      image: inputImage,
+      description: inputDescription,
+    };
+
+    alert(JSON.stringify(inputData, null, 2));
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +27,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(event) => setInputName(event.target.value)}
           />
         </label>
       </div>
@@ -22,7 +39,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(event) => setInputImage(event.target.value)}
           />
         </label>
       </div>
@@ -34,7 +51,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(event) => setInputPrice(event.target.value)}
           />
         </label>
       </div>
@@ -46,7 +63,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(event) => setInputDescription(event.target.value)}
             rows={4}
             cols={30}
           />
