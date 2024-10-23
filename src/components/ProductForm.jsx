cd34a2146234c0e217324632320bb501802085e5
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-23 23:45:25 +0700

    feat: implement feature to handle user authentication and improve error handling

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+
 function ProductForm() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let newFormData = {
+      name: name,
+      image: image,
+      price: price,
+      description: description,
+    };
+
+    alert(JSON.stringify(newFormData, null, 2));
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
+      {" "}
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +31,8 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </label>
       </div>
@@ -22,7 +44,8 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            value={image}
+            onChange={(event) => setImage(event.target.value)}
           />
         </label>
       </div>
@@ -34,7 +57,8 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
           />
         </label>
       </div>
@@ -44,9 +68,9 @@ function ProductForm() {
           <textarea
             id="description"
             name="description"
-            type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            value={description} // เชื่อมโยงกับ state
+            onChange={(event) => setDescription(event.target.value)}
             rows={4}
             cols={30}
           />
